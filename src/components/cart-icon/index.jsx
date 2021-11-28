45834b367b50d39a9563a4136e5608575ff62514
elkarouani <elkarouani@gmail.com>
2021-11-28 16:24:19 +0100

    Fix: configure relative path indexing

--- src/components/cart-icon/index.jsx
+++ src/components/cart-icon/index.jsx
@@ -1,6 +1,6 @@
 import React, { useContext } from 'react';
-import shopping from '../../assets/sale.png';
-import { CartContext } from '../../context/entities/CartContext';
+import shopping from 'assets/sale.png';
+import { CartContext } from 'context/entities/CartContext';
 import './cart-icon.styles.scss';
 
 const CartIcon = () => {
